d20e7df509f470cd1b3532ada552460d7e01e1f5
Graphics <softgiant@126.com>
2015-04-12 01:20:40 +0800

    ogl1_15_vector_font_texturing.cpp

--- ogl_1/ogl_1/src/ogl1_15_vector_font_texturing.cpp
+++ ogl_1/ogl_1/src/ogl1_15_vector_font_texturing.cpp
@@ -1,17 +1,23 @@
-//#define __exec
+#define __exec
 
 #include <stdio.h>
 #include <windows.h>
+#include <vector>
+using namespace std;
 
 #include "common_header.h"
 #include "GL/glew.h"
 #include "GL/GLU.h"
 #include "glut.h"
+#include "image.h"
 
 static HDC s_hdc;
 static HFONT s_hfont;
 static GLuint s_font_dislist;
 static GLfloat s_angle = 0.0f;
+
+vector<image_data*> s_images;
+vector<GLuint> s_tex_ids;
 
 static void init_gl() {
 	glClearColor(0, 0, 0, 1);
@@ -28,11 +34,9 @@ static void init_gl() {
 	glDepthFunc(GL_LEQUAL);
 	glEnable(GL_DEPTH_TEST);
 
-	glEnable(GL_COLOR_MATERIAL);
-
-	float l_ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f};
-	float l_diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-	float l_pos[] = { -10.0f, 10.0f, 10.0f, 1.0f };
+	float l_ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	float l_diffuse[] = { 1, 1, 1, 1 };
+	float l_pos[] = { -1.0f, 1.0f, 1.0f, 1.0f };
 	glLightfv(GL_LIGHT0, GL_AMBIENT, l_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, l_diffuse);
 	glLightfv(GL_LIGHT0, GL_POSITION, l_pos);
@@ -60,7 +64,6 @@ static void print_text_wchar_t(const wchar_t *text) {
 	SelectObject(s_hdc, s_hfont);
 	GLYPHMETRICSFLOAT gmf[1];
 	glLoadIdentity();
-	glColor3f(0.9, 0.9f, 0);
 	glTranslatef(-3.0f, 0, -10.0f);
 	glTranslatef(3.0f, 0, 0);
 	glRotatef(s_angle, 0, 1, 0);
@@ -84,6 +87,26 @@ static void on_draw() {
 	flush();
 }
 
+static void load_gl_textures() {
+	s_images.push_back(new image_data());
+	img_load_image("E:/texture1.jpg", s_images[0]);
+
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_TEXTURE_GEN_S);
+	glEnable(GL_TEXTURE_GEN_T);
+	for (int i = 0; i < s_images.size(); i++) {
+		image_data *img = s_images[i];
+		s_tex_ids.push_back(0);
+		glGenTextures(1, &s_tex_ids[i]);
+		glBindTexture(GL_TEXTURE_2D, s_tex_ids[i]);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_OBJECT_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->width, img->height, 0, img->get_gl_format(), GL_UNSIGNED_BYTE, img->pixels);
+	}
+}
+
 #ifdef __exec
 int main(int argc, char *argv[]) {
 	init_gl_window(WW, WH, "14");
@@ -91,6 +114,7 @@ int main(int argc, char *argv[]) {
 
 	init_gl();
 	build_font();
+	load_gl_textures();
 
 	glutDisplayFunc(on_draw);
 	glutIdleFunc(on_draw);
